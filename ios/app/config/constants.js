8fea56739300d7d0ffed2aaf9ff5ea0b357a8a67
vbushuev <v.bushuev@gmail.com>
2018-02-17 19:24:21 +0300

    smartVAT test app

---
+++ ios/app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = 'AIzaSyBZj9tnWp4hoMC4yyYyep9IV0cJfH5yZT8';
+export const FIREBASE_AUTH_DOMAIN = 'smartvat-368d8.firebaseapp.com';
+export const FIREBASE_DATABASE_URL = 'https://smartvat-368d8.firebaseio.com';
+export const FIREBASE_PROJECT_ID = 'smartvat-368d8';
+export const FIREBASE_STORAGE_BUCKET = 'smartvat-368d8.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '11256950549';
+
+export const FACEBOOK_APP_ID = '279698602562644';
